f8aea28587a3168661904f4b0c61fd4f8b7090a8
Jon Metzger <jmetzger@mitre.org>
2024-09-10 12:03:43 -0400

    common-password for ubuntu

--- controls/SV-260569.rb
+++ controls/SV-260569.rb
@@ -1,16 +1,16 @@
 control 'SV-260569' do
   title 'Ubuntu 22.04 LTS must store only encrypted representations of passwords.'
   desc 'Password complexity, or strength, is a measure of the effectiveness of a password in resisting attempts at guessing and brute-force attacks. If the information system or application allows the user to consecutively reuse their password when that password has exceeded its defined lifetime, the end result is a password that is not changed as per policy requirements.'
-  desc 'check', 'Verify the Ubuntu operating stores only encrypted representations of passwords with the following command: 
- 
-     $ grep pam_unix.so /etc/pam.d/common-password 
-     password [success=1 default=ignore] pam_unix.so obscure sha512 shadow remember=5 rounds=5000 
- 
+  desc 'check', 'Verify the Ubuntu operating stores only encrypted representations of passwords with the following command:
+
+     $ grep pam_unix.so /etc/pam.d/common-password
+     password [success=1 default=ignore] pam_unix.so obscure sha512 shadow remember=5 rounds=5000
+
 If "sha512" is missing from the "pam_unix.so" line, this is a finding.'
-  desc 'fix', 'Configure Ubuntu 22.04 LTS to store encrypted representations of passwords. 
- 
-Add or modify the following line in the "/etc/pam.d/common-password" file: 
- 
+  desc 'fix', 'Configure Ubuntu 22.04 LTS to store encrypted representations of passwords.
+
+Add or modify the following line in the "/etc/pam.d/common-password" file:
+
 password [success=1 default=ignore] pam_unix.so obscure sha512 shadow remember=5 rounds=5000'
   impact 0.5
   ref 'DPMS Target Canonical Ubuntu 22.04 LTS'
@@ -27,7 +27,7 @@ password [success=1 default=ignore] pam_unix.so obscure sha512 shadow remember=5
 
   pam_auth_files = input('pam_auth_files')
 
-  describe pam(pam_auth_files['system-auth']) do
+  describe pam(pam_auth_files['common-password']) do
     its('lines') { should match_pam_rule('password sufficient pam_unix.so sha512') }
   end
 end
