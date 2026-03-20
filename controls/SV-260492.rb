01a8db879a2399650ecf92af28b9a6c1b7b6361f
Princeton Wong <pwong@mitre.org>
2026-03-20 13:38:14 -0700

    update controls

--- controls/SV-260492.rb
+++ controls/SV-260492.rb
@@ -37,8 +37,8 @@ Replace "<audit_tool_name>" with the audit tool that does not have the correct p
     !%w[docker podman kubepods lxc].include?(virtualization.system)
   }
 
-  audit_tools = input('audit_tools', default: ['/sbin/auditctl', '/sbin/aureport', '/sbin/ausearch', '/sbin/autrace', '/sbin/auditd', '/sbin/augenrules'] + Dir.glob('/sbin/audispd*'))
-  audit_tool_mode = input('audit_tool_mode', default: '0755')
+  audit_tools = input('audit_tools')
+  audit_tool_mode = input('audit_tool_mode')
 
   failing_tools = audit_tools.select { |at| file(at).more_permissive_than?(audit_tool_mode) }
 
